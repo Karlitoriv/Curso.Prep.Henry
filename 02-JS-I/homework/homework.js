effa5bd4cfb1ed114d380c2cb2e98c65d45e3495
Karlitoriv <57147996+Karlitoriv@users.noreply.github.com>
2021-05-17 19:08:20 -0300

    Last changes from PC

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -69,6 +69,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
     if (str1.length === str2.length) return true;
+      return false;
 }
 
 function menosQueNoventa(num) {
