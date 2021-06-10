83352b2650b71551fa6e82522d41fc4cf76726bf
Karlitoriv <lashuestes3@gmail.com>
2021-06-10 14:19:38 -0300

    Homework JSI Terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -158,7 +158,7 @@ function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
-  //Si el número es 0, devuelve false
+  //Si el número es 0, devuelve false //
   if (numero>0) return 'Es positivo';
   if (numero<0) return 'Es negativo';
   if (numero === 0) return false;
@@ -183,6 +183,7 @@ function obtenerSaludo(nombre) {
   // "Martin" -> "Hola Martin!"
   // Tu código:
   var str = 'Hola';
+  console.log('El saludo es: ', str);
   return str + ' ' + nombre + '!';
 }
 
